b34b1e31863b43d73fe156528169806b35305820
Gidra <gidra710@gmail.com>
2025-07-14 11:48:38 +0300

    full php blog v2

--- html/blog_signup.php
+++ html/blog_signup.php
@@ -187,8 +187,8 @@
                     <h3 class="regi-form-name__title">
                         Ваше имя
                     </h3>
-                    <input class="input regi-form-name__inp" type="text" name="name" value="<?php echo
-                    $data['name']; ?>">
+                    <input class="input regi-form-name__inp" type="text" name="name" value="<?php
+                    if (isset($data['name'])) echo $data['name']; ?>"> 
                 </div>
                 <div class="flex regi-form__file">
                     <h3 class="regi-form-file__title">
@@ -200,15 +200,15 @@
                     <h3 class="regi-form-pas-1__title">
                         Ваш пароль
                     </h3>
-                    <input class="input regi-form-pas-1__inp" type="password" name="password_1"  value="<?php echo
-                    $data['password_1']; ?>">
+                    <input class="input regi-form-pas-1__inp" type="password" name="password_1"  value="<?php
+                    if (isset($data['password_1'])) echo $data['password_1']; ?>">
                 </div>
                 <div class="flex regi-form__pas-2">
                     <h3 class="regi-form-pas-2__title">
                         Введите ваш пароль ещё раз
                     </h3>
-                    <input class="input regi-form-pas-2__inp" type="password" name="password_2"  value="<?php echo
-                    $data['password_2']; ?>">
+                    <input class="input regi-form-pas-2__inp" type="password" name="password_2"  value="<?php
+                    if (isset($data['password_2'])) echo $data['password_2']; ?>">
                 </div>
                 <div class="flex regi-form__avto-autho">
                     <h3 class="regi-form-avto-autho__title">
